36dc09a61edbeb110272b0770fef1354bdf1fa69
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-27 17:52:27 +0900

    登録のテストを追加

--- taskmanager_app/spec/system/tasks_spec.rb
+++ taskmanager_app/spec/system/tasks_spec.rb
@@ -16,4 +16,32 @@ describe 'タスク管理機能', type: :system do
       expect(page).to have_content('最初のタスク')
     end
   end
+
+  describe '新規登録機能' do
+    attribute_name = Task.human_attribute_name(:name)
+
+    before do
+      visit(new_task_path)
+      fill_in(attribute_name, with: task_name)
+      click_button(I18n.t('helpers.submit.create'))
+    end
+
+    context '名前を渡した場合' do
+      let(:task_name) { '適当な名前' }
+
+      it '新規登録できる' do
+        expect(page).to have_selector '.alert-success', text: '適当な名前'
+      end
+    end
+
+    context '名前を渡さなかった場合' do
+      let(:task_name) { '' }
+
+      it 'エラーになる' do
+        within('#error_explanation') do
+          expect(page).to have_content("#{attribute_name}を入力してください")
+        end
+      end
+    end
+  end
 end
