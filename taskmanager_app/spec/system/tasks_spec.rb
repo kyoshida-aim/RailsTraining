ff881286072e60e8242698f78e7a19b9a205071b
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-27 16:50:48 +0900

    詳細表示のテストを追加

--- taskmanager_app/spec/system/tasks_spec.rb
+++ taskmanager_app/spec/system/tasks_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
+  let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク') }
+
   describe '一覧表示機能' do
-    context '最初のタスクが存在している時' do
-      before do
-        FactoryBot.create(:task, name: '最初のタスク')
-      end
-      it '一覧表示のページで最初のタスクが表示される' do
-        visit(tasks_path)
-        expect(page).to have_content('最初のタスク')
-      end
+    it '一覧表示のページで最初のタスクが表示される' do
+      visit(tasks_path)
+      expect(page).to have_content('最初のタスク')
+    end
+  end
+
+  describe '詳細表示機能' do
+    it '最初のタスクの詳細が表示される' do
+      visit(tasks_path(task_a))
+      expect(page).to have_content('最初のタスク')
     end
   end
 end
