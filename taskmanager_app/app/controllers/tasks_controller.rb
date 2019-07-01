50563037bfccc471788f37792cb3a98d562cef11
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-01 15:51:05 +0900

    Rails/SaveBangの修正

--- taskmanager_app/app/controllers/tasks_controller.rb
+++ taskmanager_app/app/controllers/tasks_controller.rb
@@ -17,13 +17,13 @@ class TasksController < ApplicationController
 
   def update
     task = Task.find(params[:id])
-    task.update(task_params)
+    task.update!(task_params)
     redirect_to(task_url, notice: t("helpers.edit.notice", name: task.name))
   end
 
   def destroy
     task = Task.find(params[:id])
-    task.destroy
+    task.destroy!
     redirect_to(tasks_url, notice: t("helpers.delete.notice", name: task.name))
   end
 
