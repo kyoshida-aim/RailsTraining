235e0ddd34b61025cffecc2f1e55df68d824762a
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-01 15:56:42 +0900

    privateの後のメソッドはインデントを下げるようです

--- taskmanager_app/app/controllers/tasks_controller.rb
+++ taskmanager_app/app/controllers/tasks_controller.rb
@@ -38,7 +38,7 @@ class TasksController < ApplicationController
 
   private
 
-  def task_params
-    params.require(:task).permit(:name, :description)
-  end
+    def task_params
+      params.require(:task).permit(:name, :description)
+    end
 end
