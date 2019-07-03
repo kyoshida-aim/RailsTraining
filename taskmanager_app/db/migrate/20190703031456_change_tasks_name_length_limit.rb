202ed0760e1d20ad19f01367856c159ca3c9a916
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-03 12:37:38 +0900

    名称の長さを制限するマイグレーションを追加

---
+++ taskmanager_app/db/migrate/20190703031456_change_tasks_name_length_limit.rb
@@ -0,0 +1,9 @@
+class ChangeTasksNameLengthLimit < ActiveRecord::Migration[5.2]
+  def up
+    change_column(:tasks, :name, :string, limit: 30)
+  end
+
+  def down
+    change_column(:tasks, :name, :string)
+  end
+end
