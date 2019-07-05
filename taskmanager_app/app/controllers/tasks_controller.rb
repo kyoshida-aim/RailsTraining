1b07d356f6b3e620a4acc556f61dd5c79ed219ba
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-05 16:40:12 +0900

    終了期日でソートできるように

--- taskmanager_app/app/controllers/tasks_controller.rb
+++ taskmanager_app/app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all.order(created_at: :desc)
+    # ransackにより受け渡されるパラメータ:qによってソート
+    @q = Task.all.order(created_at: :desc).ransack(params[:q])
+    @tasks = @q.result
   end
 
   def show
