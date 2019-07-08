f5c0550ad9b5853a9a0e708f6799d96fb8b5b57c
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-08 18:32:44 +0900

    一覧画面に検索機能を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,12 @@ class Task < ApplicationRecord
   enum status: I18n.t(["not_started", "in_progress", "finished"], scope: "activerecord.attributes.task")
 
   validates :name, presence: true, length: { maximum: 30 }
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name status deadline]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
