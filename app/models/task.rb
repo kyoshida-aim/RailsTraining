8456af083a23d2a51eb86dca12473d7d3bbeb16b
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-08 17:33:48 +0900

    Taskモデルにステータスカラムを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
+  enum status: I18n.t(["not_started", "in_progress", "finished"], scope: "activerecord.attributes.task")
+
   validates :name, presence: true, length: { maximum: 30 }
 end
