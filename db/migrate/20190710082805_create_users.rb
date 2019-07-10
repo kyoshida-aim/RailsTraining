a77ffc7eeccbe634d564f38321dc4448a697d0be
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-10 17:50:14 +0900

    rubocop -a --only Layout/SpaceAfterColon

--- db/migrate/20190710082805_create_users.rb
+++ db/migrate/20190710082805_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :login_id, null:false
-      t.string :password_digest, null:false
+      t.string :login_id, null: false
+      t.string :password_digest, null: false
 
       t.timestamps
       t.index :login_id, unique: true
