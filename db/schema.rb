44a56d028d38b124a54b70fbf975ee61e22f6c86
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-10 19:00:05 +0900

    タスクにユーザーを紐づける

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_082805) do
+ActiveRecord::Schema.define(version: 2019_07_10_091902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,9 @@ ActiveRecord::Schema.define(version: 2019_07_10_082805) do
     t.datetime "deadline"
     t.integer "status", default: 0
     t.integer "priority", default: 1, null: false
+    t.bigint "user_id", default: 0, null: false
     t.index ["status"], name: "index_tasks_on_status"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
