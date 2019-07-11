572a71eca9b7982319f44835aa8a4ff2f360ea2b
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-11 14:19:43 +0900

    Rails/NotNullColumnを無効化

--- db/migrate/20190710091902_add_userid_to_task.rb
+++ db/migrate/20190710091902_add_userid_to_task.rb
@@ -1,5 +1,5 @@
 class AddUseridToTask < ActiveRecord::Migration[5.2]
   def change
-    add_reference(:tasks, :user, null: false, index: true, default: 0)
+    add_reference(:tasks, :user, null: false, index: true)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2019_07_10_091902) do
     t.datetime "deadline"
     t.integer "status", default: 0
     t.integer "priority", default: 1, null: false
-    t.bigint "user_id", default: 0, null: false
+    t.bigint "user_id", null: false
     t.index ["status"], name: "index_tasks_on_status"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
