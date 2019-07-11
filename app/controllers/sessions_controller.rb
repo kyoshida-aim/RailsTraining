2566402b11bc623c27b185431ad5f48b751926dd
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-11 18:46:05 +0900

    ログイン画面の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(login_id: session_params[:login_id])
+
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to(root_path, notice: t("helpers.login.success"))
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def session_params
+      params.require(:session).permit(:login_id, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "sessions/new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
   root to: "tasks#index"
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
