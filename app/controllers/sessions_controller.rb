01125a0ac9c26c0b5ace3beadffc2cccc0e25d14
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-12 15:10:36 +0900

    ログイン失敗時にアラートを表示

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,8 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to(root_path, notice: t(".login.success"))
     else
-      render :new
+      flash[:warning] = t(".login.failed")
+      render(:new)
     end
   end
 
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -4,13 +4,53 @@ describe "ユーザー関連機能", type: :system do
   describe "ログイン" do
     let!(:user) { FactoryBot.create(:user, login_id: "ユーザーA") }
 
-    it "ログインできる" do
-      visit(login_path)
-      fill_in(with: user.login_id, id: "session_login_id")
-      fill_in(with: user.password, id: "session_password")
-      click_button(I18n.t("helpers.submit.login"))
+    context "ユーザーIDとパスワードを正しく入力した場合" do
+      it "ログインできる" do
+        visit(login_path)
+        fill_in(with: user.login_id, id: "session_login_id")
+        fill_in(with: user.password, id: "session_password")
+        click_button(I18n.t("helpers.submit.login"))
 
-      expect(page).to have_selector(".alert-success", text: "ログインしました")
+        expect(page).to have_selector(".alert-success", text: "ログインしました")
+      end
+    end
+
+    context "存在しないユーザーでログインしようとした場合" do
+      it "ログインできない" do
+        visit(login_path)
+        fill_in(with: "不正なユーザー名", id: "session_login_id")
+        fill_in(with: "不正なパスワード", id: "session_password")
+        click_button(I18n.t("helpers.submit.login"))
+
+        expect(page).to have_selector(".alert-warning", text: "ログインに失敗しました")
+      end
+    end
+
+    describe "正しく入力しないユーザーをログインさせない" do
+      before do
+        visit(login_path)
+        fill_in(with: login_id, id: "session_login_id")
+        fill_in(with: password, id: "session_password")
+        click_button(I18n.t("helpers.submit.login"))
+      end
+
+      context "ユーザーIDが入力されていない場合" do
+        let(:login_id) { "" }
+        let(:password) { user.password }
+
+        it "ログインできない" do
+          expect(page).to have_selector(".alert-warning", text: "ログインに失敗しました")
+        end
+      end
+
+      context "パスワードが入力されていない場合" do
+        let(:login_id) { user.login_id }
+        let(:password) { "" }
+
+        it "ログインできない" do
+          expect(page).to have_selector(".alert-warning", text: "ログインに失敗しました")
+        end
+      end
     end
 
     context "ログインしていない場合" do
