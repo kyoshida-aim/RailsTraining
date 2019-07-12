13f707f4723c1a589c6a52929efe1e190e15f2b4
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-12 15:35:14 +0900

    [Fix]Flashメッセージがリロードで正しく消えなかったため修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to(root_path, notice: t(".login.success"))
     else
-      flash[:warning] = t(".login.failed")
+      flash.now[:warning] = t(".login.failed")
       render(:new)
     end
   end
