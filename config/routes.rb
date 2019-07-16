97579a5814746d613078fd3df12bb930194bdb65
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-16 12:45:02 +0900

    bin/rails g controller Admin::Users new edit show index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get "users/new"
+    get "users/edit"
+    get "users/show"
+    get "users/index"
+  end
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
