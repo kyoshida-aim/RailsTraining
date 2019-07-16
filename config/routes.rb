f988c782f4d659ef1c41cdd36aa9e11526170782
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-16 14:08:25 +0900

    管理画面のresource設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get "users/new"
-    get "users/edit"
-    get "users/show"
-    get "users/index"
+    resources :users
   end
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
