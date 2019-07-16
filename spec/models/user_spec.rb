aa8c24609fe79e9601396f699ed30a5c56252c60
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-16 16:34:50 +0900

    管理者権限の追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
-# require "rails_helper"
+require "rails_helper"
 
-# describe User, type: :model do
-#   # write test here.
-# end
+describe User, type: :model do
+  describe "管理者権限" do
+    let(:user) { FactoryBot.create(:user) }
+
+    context "ユーザーの管理権限の初期設定" do
+      it "管理権限を持たない" do
+        expect(user.admin).to eq(false)
+      end
+    end
+  end
+end
