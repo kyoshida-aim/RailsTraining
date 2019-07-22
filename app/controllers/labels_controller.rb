4fd0ac9f3f50dc0c617197bcb37b8449123ebbc9
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-22 18:05:52 +0900

    bin/rails g controller labels index show new edit
    ルーティングのみ編集

---
+++ app/controllers/labels_controller.rb
@@ -0,0 +1,13 @@
+class LabelsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :tasks, only: [:index, :show]
     end
   end
+  resources :labels
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
