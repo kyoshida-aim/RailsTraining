5e0074ec065d15dc5a266ca291f246211e167655
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-25 14:31:58 +0900

    関数名をより適切なものに変更

--- app/models/label.rb
+++ app/models/label.rb
@@ -4,9 +4,9 @@ class Label < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, length: { maximum: NAME_SIZE_MAX }
 
-  before_validation :remove_whitespaces
+  before_validation :strip_whitespaces
 
-  def remove_whitespaces
+  def strip_whitespaces
     name.strip!
   end
 end
