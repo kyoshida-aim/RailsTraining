9a4056dfcc35d98d56bb483572f1388a1932b0e0
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-25 14:27:48 +0900

    FactoryBotの修正:login_idの文字数上限を設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ require "faker"
 
 FactoryBot.define do
   factory :user do
-    login_id { Faker::Internet.unique.username(4, []) }
+    login_id { Faker::Internet.unique.username(4..20, []) }
     password { Faker::Internet.password }
   end
 end
