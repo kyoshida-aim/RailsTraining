ddd03c5b5b664d425189522bace33ec27a77df84
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-07-26 20:29:20 +0900

    管理画面でのタスク一覧からラベルの検索を行うテスト

--- spec/system/admin/tasks_spec.rb
+++ spec/system/admin/tasks_spec.rb
@@ -17,4 +17,26 @@ describe "管理者/タスク一覧", type: :system do
       expect(page).to have_current_path(Regexp.new(expected_path))
     end
   end
+
+  describe "ラベルでの検索" do
+    let!(:task) { FactoryBot.create_list(:task, 3, user: user) }
+    let!(:label) { FactoryBot.create_list(:label, 3, user: user) }
+
+    before do
+      task[0].labels = [label[0], label[1]]
+      task[1].labels = [label[1]]
+      task[2].labels = [label[2]]
+    end
+
+    it "指定したラベルをもつタスクのみが表示される" do
+      visit(admin_user_tasks_path(user))
+      find(class: /search-form label_id-#{label[1].id}/).check
+      click_on(I18n.t("helpers.submit.search"))
+      tasks = all(id: /\Atask-name-(\d+)\z/).collect(&:text)
+
+      expect(tasks).to include(task[0].name)
+      expect(tasks).to include(task[1].name)
+      expect(tasks).not_to include(task[2].name)
+    end
+  end
 end
